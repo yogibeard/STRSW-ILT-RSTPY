fce2f93e6907517a2ede32a85e5df20101b4cc4e
yogibeard <84163092+yogibeard@users.noreply.github.com>
2024-07-24 12:42:24 +0800

    202407241241 Added Demo for Mod 1,2,3

--- cluster.py
+++ cluster.py
@@ -2,9 +2,8 @@
 from netapp_ontap import config
 from netapp_ontap import HostConnection
 from netapp_ontap.resources import Cluster
-import urllib3
 
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
 conn = HostConnection("192.168.0.101", username = "admin", password = "Netapp1!", verify = False)
 config.CONNECTION = conn
 clus = Cluster()
